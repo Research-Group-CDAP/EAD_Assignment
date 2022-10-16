31ab7395b47cae13169eb74aba14d16108700a46
rusiruavb <rusiruavb98@gmail.com>
2022-10-16 21:13:28 +0530

    Add Vehicle to Queue and exit functions implemented

---
+++ Backend/controller/FuelStation.controller.js
@@ -0,0 +1,84 @@
+const User = require("../models/User.model");
+const FuelStation = require("../models/FuelStation.model");
+
+//get Queue Details Fuel Station
+const getQueueDetailsFuelStation = async (req, res) => {
+  try {
+    const queueDetails = await FuelStation.findOne({
+      fuelStationName: req.params.fuelStationName,
+    });
+    res.json(queueDetails);
+  } catch {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+const addVehicleIntoFuelStation = async (request, response) => {
+  return await FuelStation.findById(request.params.fuelStationName)
+    .then(async (FuelStationDetails) => {
+      if (FuelStationDetails) {
+        let vehicleDetails = {
+          vehicleType: request.body.vehicleType,
+          vehicleNumber: request.body.vehicleNumber,
+          inTime: new Date(),
+        };
+
+        FuelStationDetails.presentVehicleLogs =
+          FuelStationDetails.presentVehicleLogs.push(vehicleDetails);
+
+        return await FuelStationDetails.save()
+          .then((updatedFuelStation) => {
+            return response.json(updatedFuelStation);
+          })
+          .catch((error) => {
+            return response.json(error);
+          });
+      } else {
+        return response.json("FuelStation Not Found");
+      }
+    })
+    .catch((error) => {
+      return response.json(error);
+    });
+};
+
+const exitVehiclefromFuelStation = async (request, response) => {
+  return await FuelStation.findById(request.params.fuelStationName)
+    .then(async (FuelStationDetails) => {
+      if (FuelStationDetails) {
+
+        FuelStationDetails.presentVehicleLogs = FuelStationDetails.presentVehicleLogs.filter(function (vehicle) {
+          return vehicle.vehicleNumber !== request.body.vehicleNumber;
+        });
+
+        let vehicleDetails = {
+          vehicleType: request.body.vehicleType,
+          vehicleNumber: request.body.vehicleNumber,
+          outTime: new Date(),
+        };
+
+        FuelStationDetails.pastVehicleLogs =
+          FuelStationDetails.pastVehicleLogs.push(vehicleDetails);
+
+        return await FuelStationDetails.save()
+          .then((updatedFuelStation) => {
+            return response.json(updatedFuelStation);
+          })
+          .catch((error) => {
+            return response.json(error);
+          });
+      } else {
+        return response.json("FuelStation Not Found");
+      }
+    })
+    .catch((error) => {
+      return response.json(error);
+    });
+};
+
+module.exports = {
+  getQueueDetailsFuelStation,
+  addVehicleIntoFuelStation,
+  exitVehiclefromFuelStation,
+};
